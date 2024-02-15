8cd78eacd85bbf96039460d9dcd5105847aba2bf
I Made Ocy Darma Putra <ochi@troke.id>
2024-02-15 22:37:38 +0700

    feat: schema var no_consumer changed to name variable

--- src/schema.lua
+++ src/schema.lua
@@ -2,7 +2,7 @@ local function check_for_body_replace_pattern(value)
     for _, body_replace_pattern in ipairs(value) do
         local found = string.find(body_replace_pattern, "###")
         if not found then
-            return false, "body_replace_pattern '" .. body_replace_pattern .. "' has no ### seperator."
+            return false, "body_replace_pattern '" .. body_replace_pattern .. "' has no ### separator."
         end
     end
     return true
@@ -12,14 +12,14 @@ local function check_for_header_replace_pattern(value)
     for _, header_replace_pattern in ipairs(value) do
         local found = header_replace_pattern:match("^(.+):(.+)###(.*)$")
         if not found then
-            return false, "header_replace_pattern '" .. header_replace_pattern .. "' must have a ':' and a '###' seperator."
+            return false, "header_replace_pattern '" .. header_replace_pattern .. "' must have a ':' and a '###' separator."
         end
     end
     return true
 end
 
 return {
-  no_consumer = true,
+  name = "kong-response-string-replace",
   fields = {
       content_types = {type = "array", default = {}},
       uri_patterns = {type = "array", default = {}},
