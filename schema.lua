6427d7c43facbe156886f95ee313b162d438f685
ocy-darma <imadeocy@itsecasia.com>
2024-02-22 18:58:30 +0700

    remove body and header

--- schema.lua
+++ schema.lua
@@ -1,18 +1,8 @@
-local function check_for_body_replace_pattern(value)
-    for _, body_replace_pattern in ipairs(value) do
-        local found = string.find(body_replace_pattern, "###")
+local function check_for_replace_pattern(value)
+    for _, replace_pattern in ipairs(value) do
+        local found = string.find(replace_pattern, "###")
         if not found then
-            return false, "body_replace_pattern '" .. body_replace_pattern .. "' has no ### separator."
-        end
-    end
-    return true
-end
-
-local function check_for_header_replace_pattern(value)
-    for _, header_replace_pattern in ipairs(value) do
-        local found = header_replace_pattern:match("^(.+):(.+)###(.*)$")
-        if not found then
-            return false, "header_replace_pattern '" .. header_replace_pattern .. "' must have a ':' and a '###' separator."
+            return false, "Replace pattern '" .. replace_pattern .. "' must contain '###' separator."
         end
     end
     return true
@@ -21,9 +11,9 @@ end
 return {
   name = "kong-response-string-replace",
   fields = {
-      content_types = {type = "array", default = {}},
-      uri_patterns = {type = "array", default = {}},
-      body_replace_patterns = {type = "array", default = {}, func = check_for_body_replace_pattern },
-      header_replace_patterns = {type = "array", default = {}, func = check_for_header_replace_pattern }
+    { content_types = { type = "array", default = {} } },
+    { uri_patterns = { type = "array", default = {} } },
+    { body_replace_patterns = { type = "array", default = {}, func = check_for_replace_pattern } },
+    { header_replace_patterns = { type = "array", default = {}, func = check_for_replace_pattern } }
   }
 }
